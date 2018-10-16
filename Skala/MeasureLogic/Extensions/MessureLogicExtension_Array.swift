67adeac25846bd24d795bb79cf55e43725c4953c
JohannesGHeinke <johannes.heinke@mikavaa.com>
2018-10-16 13:22:17 +0200

    tmp

--- Skala/MeasureLogic/Extensions/MessureLogicExtension_Array.swift
+++ Skala/MeasureLogic/Extensions/MessureLogicExtension_Array.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+internal extension Array where Element == SCNVector3 {
+    internal var average: SCNVector3? {
+        guard !self.isEmpty else {
+            return nil
+        }
+        let total = self.reduce(SCNVector3Zero) { (tmp, vector) -> SCNVector3 in
+            return SCNVector3.init(tmp.x + vector.x, tmp.y + vector.y, tmp.z + vector.z)
+        }
+        return SCNVector3.init(total.x / Float(self.count), total.y / Float(self.count), total.z / Float(self.count))
+    }
+}
