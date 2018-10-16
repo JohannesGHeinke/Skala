d992ac510d38a5c89eafa195c7c19efe1354475b
JohannesGHeinke <johannes.heinke@mikavaa.com>
2018-10-16 13:17:20 +0200

    update

--- Skala/MeasureLogic/Extensions/MeasureLogicExtension_ARSCNView.swift
+++ Skala/MeasureLogic/Extensions/MeasureLogicExtension_ARSCNView.swift
@@ -9,6 +9,313 @@
 import Foundation
 import ARKit
 
+// MARK: - SCNNode extension
+extension SCNNode {
+    
+    func setUniformScale(_ scale: Float) {
+        self.scale = SCNVector3Make(scale, scale, scale)
+    }
+    
+    func renderOnTop() {
+        self.renderingOrder = 2
+        if let geom = self.geometry {
+            for material in geom.materials {
+                material.readsFromDepthBuffer = false
+            }
+        }
+        for child in self.childNodes {
+            child.renderOnTop()
+        }
+    }
+    
+    func setPivot() {
+        let minVec = self.boundingBox.min
+        let maxVec = self.boundingBox.max
+        let bound = SCNVector3Make( maxVec.x - minVec.x, maxVec.y - minVec.y, maxVec.z - minVec.z);
+        self.pivot = SCNMatrix4MakeTranslation(bound.x / 2, bound.y, bound.z / 2);
+    }
+}
+
+// MARK: - SCNVector3 extensions
+extension SCNVector3 {
+    
+    init(_ vec: vector_float3) {
+        self.x = vec.x
+        self.y = vec.y
+        self.z = vec.z
+    }
+    
+    func length() -> Float {
+        return sqrtf(x * x + y * y + z * z)
+    }
+    
+    func distanceFromPos(pos: SCNVector3) -> Float {
+        let diff = SCNVector3(self.x - pos.x, self.y - pos.y, self.z - pos.z);
+        return diff.length()
+    }
+    
+    mutating func setLength(_ length: Float) {
+        self.normalize()
+        self *= length
+    }
+    
+    mutating func setMaximumLength(_ maxLength: Float) {
+        if self.length() <= maxLength {
+            return
+        } else {
+            self.normalize()
+            self *= maxLength
+        }
+    }
+    
+    mutating func normalize() {
+        self = self.normalized()
+    }
+    
+    func normalized() -> SCNVector3 {
+        if self.length() == 0 {
+            return self
+        }
+        
+        return self / self.length()
+    }
+    
+    static func positionFromTransform(_ transform: matrix_float4x4) -> SCNVector3 {
+        return SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+    }
+    
+    func friendlyString() -> String {
+        return "(\(String(format: "%.2f", x)), \(String(format: "%.2f", y)), \(String(format: "%.2f", z)))"
+    }
+    
+    func dot(_ vec: SCNVector3) -> Float {
+        return (self.x * vec.x) + (self.y * vec.y) + (self.z * vec.z)
+    }
+    
+    func cross(_ vec: SCNVector3) -> SCNVector3 {
+        return SCNVector3(self.y * vec.z - self.z * vec.y, self.z * vec.x - self.x * vec.z, self.x * vec.y - self.y * vec.x)
+    }
+}
+
+public let SCNVector3One: SCNVector3 = SCNVector3(1.0, 1.0, 1.0)
+
+func SCNVector3Uniform(_ value: Float) -> SCNVector3 {
+    return SCNVector3Make(value, value, value)
+}
+
+func SCNVector3Uniform(_ value: CGFloat) -> SCNVector3 {
+    return SCNVector3Make(Float(value), Float(value), Float(value))
+}
+
+func + (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
+
+func - (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x - right.x, left.y - right.y, left.z - right.z)
+}
+
+func += (left: inout SCNVector3, right: SCNVector3) {
+    left = left + right
+}
+
+func -= (left: inout SCNVector3, right: SCNVector3) {
+    left = left - right
+}
+
+func / (left: SCNVector3, right: Float) -> SCNVector3 {
+    return SCNVector3Make(left.x / right, left.y / right, left.z / right)
+}
+
+func * (left: SCNVector3, right: Float) -> SCNVector3 {
+    return SCNVector3Make(left.x * right, left.y * right, left.z * right)
+}
+
+func /= (left: inout SCNVector3, right: Float) {
+    left = left / right
+}
+
+func *= (left: inout SCNVector3, right: Float) {
+    left = left * right
+}
+
+// MARK: - SCNMaterial extensions
+extension SCNMaterial {
+    
+    static func material(withDiffuse diffuse: Any?, respondsToLighting: Bool = true) -> SCNMaterial {
+        let material = SCNMaterial()
+        material.diffuse.contents = diffuse
+        material.isDoubleSided = true
+        if respondsToLighting {
+            material.locksAmbientWithDiffuse = true
+        } else {
+            material.ambient.contents = UIColor.black
+            material.lightingModel = .constant
+            material.emission.contents = diffuse
+        }
+        return material
+    }
+}
+
+// MARK: - Collection extensions
+extension Array where Iterator.Element == CGFloat {
+    var average: CGFloat? {
+        guard !isEmpty else {
+            return nil
+        }
+        
+        var ret = self.reduce(CGFloat(0)) { (cur, next) -> CGFloat in
+            var cur = cur
+            cur += next
+            return cur
+        }
+        let fcount = CGFloat(count)
+        ret /= fcount
+        return ret
+    }
+}
+
+
+extension RangeReplaceableCollection where IndexDistance == Int {
+    mutating func keepLast(_ elementsToKeep: Int) {
+        if count > elementsToKeep {
+            self.removeFirst(count - elementsToKeep)
+        }
+    }
+}
+
+// MARK: - CGPoint extensions
+extension CGPoint {
+    
+    init(_ size: CGSize) {
+        self.x = size.width
+        self.y = size.height
+    }
+    
+    init(_ vector: SCNVector3) {
+        self.x = CGFloat(vector.x)
+        self.y = CGFloat(vector.y)
+    }
+    
+    func distanceTo(_ point: CGPoint) -> CGFloat {
+        return (self - point).length()
+    }
+    
+    func length() -> CGFloat {
+        return sqrt(self.x * self.x + self.y * self.y)
+    }
+    
+    func midpoint(_ point: CGPoint) -> CGPoint {
+        return (self + point) / 2
+    }
+    
+    func friendlyString() -> String {
+        return "(\(String(format: "%.2f", x)), \(String(format: "%.2f", y)))"
+    }
+}
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func += (left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+func -= (left: inout CGPoint, right: CGPoint) {
+    left = left - right
+}
+
+func / (left: CGPoint, right: CGFloat) -> CGPoint {
+    return CGPoint(x: left.x / right, y: left.y / right)
+}
+
+func * (left: CGPoint, right: CGFloat) -> CGPoint {
+    return CGPoint(x: left.x * right, y: left.y * right)
+}
+
+func /= (left: inout CGPoint, right: CGFloat) {
+    left = left / right
+}
+
+func *= (left: inout CGPoint, right: CGFloat) {
+    left = left * right
+}
+
+// MARK: - CGSize extensions
+extension CGSize {
+    
+    init(_ point: CGPoint) {
+        self.width = point.x
+        self.height = point.y
+    }
+    
+    func friendlyString() -> String {
+        return "(\(String(format: "%.2f", width)), \(String(format: "%.2f", height)))"
+    }
+}
+
+func + (left: CGSize, right: CGSize) -> CGSize {
+    return CGSize(width: left.width + right.width, height: left.height + right.height)
+}
+
+func - (left: CGSize, right: CGSize) -> CGSize {
+    return CGSize(width: left.width - right.width, height: left.height - right.height)
+}
+
+func += (left: inout CGSize, right: CGSize) {
+    left = left + right
+}
+
+func -= (left: inout CGSize, right: CGSize) {
+    left = left - right
+}
+
+func / (left: CGSize, right: CGFloat) -> CGSize {
+    return CGSize(width: left.width / right, height: left.height / right)
+}
+
+func * (left: CGSize, right: CGFloat) -> CGSize {
+    return CGSize(width: left.width * right, height: left.height * right)
+}
+
+func /= (left: inout CGSize, right: CGFloat) {
+    left = left / right
+}
+
+func *= (left: inout CGSize, right: CGFloat) {
+    left = left * right
+}
+
+// MARK: - CGRect extensions
+extension CGRect {
+    
+    var mid: CGPoint {
+        return CGPoint(x: midX, y: midY)
+    }
+}
+
+private func rayIntersectionWithHorizontalPlane(rayOrigin: SCNVector3, direction: SCNVector3, planeY: Float) -> SCNVector3? {
+    let direction = direction.normalized()
+    if direction.y == 0 {
+        if rayOrigin.y == planeY {
+            return rayOrigin
+        } else {
+            return nil
+        }
+    }
+    let dist = (planeY - rayOrigin.y) / direction.y
+    if dist < 0 {
+        return nil
+    }
+    return rayOrigin + (direction * dist)
+}
+
+
 extension ARSCNView {
     func worldPositionFromScreenPosition(_ position: CGPoint,
                                          objectPos: SCNVector3?,
